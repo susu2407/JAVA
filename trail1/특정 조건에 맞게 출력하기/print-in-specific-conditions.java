368189e485aabcfe151cf219fbfdfe0e7fc07ec5
susu <tndus159200297@gmail.com>
2026-06-05 17:37:00 +0900

    [Passed] 특정 조건에 맞게 출력하기 (137ms, 10MB)

--- trail1/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
+++ trail1/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
@@ -10,18 +10,17 @@ public class Main {
         for (int i = 0; i < 100; i++) {
             arr[i] = sc.nextInt();
 
+            if (arr[i] == 0) {
+                break;
+            }
+
             if (arr[i] % 2 == 1) {
                 arr[i] = arr[i] + 3;
             } else if (arr[i] % 2 == 0) {
                 arr[i] = arr[i] / 2;
             }
 
-            if (arr[i] != 0) {
-                System.out.print(arr[i] + " ");
-            } else {
-                break;
-            }
-
+            System.out.print(arr[i] + " ");
         }
     }
 }
