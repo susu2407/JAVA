e0a4d4bb03dd77b294c21a743bf668de6245bd9a
susu <tndus159200297@gmail.com>
2026-06-05 16:48:55 +0900

    [Passed] 숫자들의 배수 (150ms, 10MB)

--- trail1/숫자들의 배수/multiple-of-numbers.java
+++ trail1/숫자들의 배수/multiple-of-numbers.java
@@ -4,22 +4,22 @@ public class Main {
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
-
-        int [] arr = new int[10];
-
+    
+        int[] arr = new int[10];
         int N = sc.nextInt();
+
+        int count = 0; 
 
         for (int i = 0; i < 10; i++) {
             arr[i] = N * (i + 1);
-
             System.out.print(arr[i] + " ");
 
             if (arr[i] % 5 == 0) {
-                if (arr[i] == N * 2) {
-                    break;
-                } else if (arr[i] / (N * 5 ) == 2) {
-                    break;
-                }
+                count++;
+            }
+
+            if (count == 2) {
+                break;
             }
         }
     }
