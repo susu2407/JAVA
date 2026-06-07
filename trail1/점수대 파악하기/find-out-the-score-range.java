5c9c83577220abffcb4a5ae97e65254b6523e851
susu <tndus159200297@gmail.com>
2026-06-07 20:50:23 +0900

    [Passed] 점수대 파악하기 (138ms, 10MB)

--- trail1/점수대 파악하기/find-out-the-score-range.java
+++ trail1/점수대 파악하기/find-out-the-score-range.java
@@ -6,7 +6,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int[] score = new int[100];
-        int[] scoreCount = new int[10];
+        int[] scoreCount = new int[11];
 
         for (int i = 0; i < 100; i++) {
             score[i] = sc.nextInt();
@@ -15,42 +15,11 @@ public class Main {
                 break;
             }
 
-            switch (score[i] / 10) {
-                case 10:
-                    scoreCount[9]++;
-                    break;
-                case 9:
-                    scoreCount[8]++;
-                    break;
-                case 8:
-                    scoreCount[7]++;
-                    break;
-                case 7:
-                    scoreCount[6]++;
-                    break;
-                case 6:
-                    scoreCount[5]++;
-                    break;
-                case 5:
-                    scoreCount[4]++;
-                    break;
-                case 4:
-                    scoreCount[3]++;
-                    break;
-                case 3:
-                    scoreCount[2]++;
-                    break;
-                case 2:
-                    scoreCount[1]++;
-                    break;
-                case 1:
-                    scoreCount[0]++;
-                    break;
-            }
+            scoreCount[score[i] / 10]++;
         }
 
-        for (int i = 9; i >= 0; i--) {
-            System.out.println( (i + 1) * 10 + " - " + scoreCount[i]);
+        for (int i = 10; i > 0; i--) {
+            System.out.println( i * 10 + " - " + scoreCount[i]);
         }
     }
 }
