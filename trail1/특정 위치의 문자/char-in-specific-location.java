7065b130f12314f7d3cfc910957ba4eabb797698
susu <tndus159200297@gmail.com>
2026-06-07 23:30:22 +0900

    [Passed] 특정 위치의 문자 (132ms, 10MB)

--- trail1/특정 위치의 문자/char-in-specific-location.java
+++ trail1/특정 위치의 문자/char-in-specific-location.java
@@ -7,15 +7,20 @@ public class Main {
 
         char[] word = new char[] {'L', 'E', 'B', 'R', 'O', 'S'};
 
-        char input = sc.next().charAt(0);
+        char serch = sc.next().charAt(0)
+;
+        int exis = -1;
 
         for (int i = 0; i < 6; i++) {
-            if (word[i] == input) {
-                System.out.println(i);
-                break;
-            } else {
-                System.out.println("None");
+            if (word[i] == serch) {
+                exis = i;
             }
+        }
+
+        if (exis == -1) {
+            System.out.println("None");
+        } else {
+            System.out.println(exis);
         }
     }
 }
