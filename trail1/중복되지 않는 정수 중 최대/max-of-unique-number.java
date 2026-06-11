209789fa51c13d20c4ddc564099bf41e98ad7b4a
susu <tndus159200297@gmail.com>
2026-06-11 23:51:02 +0900

    [Runtime Error] 중복되지 않는 정수 중 최대 (128ms, 10MB)

---
+++ trail1/중복되지 않는 정수 중 최대/max-of-unique-number.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+        int[] count = new int[1000];
+
+        int maxVal = -1;
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+            count[arr[i]]++;
+        }
+
+        for (int i = 0; i < N; i++) {
+            int currentVal = arr[i];
+
+            if (count[currentVal] == 1 && arr[i] > maxVal) {
+                maxVal = arr[i];
+            }
+            
+        }
+        System.out.println(maxVal);
+    }
+}
