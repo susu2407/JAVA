d37f5df7b038ef43bd765414b31bf41673bb0591
susu <tndus159200297@gmail.com>
2026-06-16 23:17:46 +0900

    [Passed] 대문자로 바꾸기 (126ms, 10MB)

--- trail1/대문자로 바꾸기/change-to-capital.java
+++ trail1/대문자로 바꾸기/change-to-capital.java
@@ -9,10 +9,9 @@ public class Main {
 
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 3; j++) {
-                arr2d[i][j] = sc.next().charAt(0);
 
                 // 문자 하나의 소문자를 대문자로 바꾸는 메서드: Character.toUpperCase()
-                arr2d[i][j] = Character.toUpperCase(arr2d[i][j]);
+                arr2d[i][j] = Character.toUpperCase(sc.next().charAt(0));
 
                 System.out.print(arr2d[i][j] + " ");
             }
