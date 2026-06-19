a87a5390a5b8cdb1e895d25e080906a9e2f0e77a
susu <tndus159200297@gmail.com>
2026-06-19 21:55:39 +0900

    [Runtime Error] 정수 직사각형 (131ms, 11MB)

---
+++ trail1/정수 직사각형/rectangle-of-numbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();   // 행 개수
+        int M = sc.nextInt();   // 열 개수
+
+        int [][] arr2d = new int[N][M];
+    
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                arr2d[i][j] = sc.nextInt();
+
+                System.out.print(arr2d[i][j] + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
