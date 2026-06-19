a1ede320c4409ab33f5e4b7ee8bd1aa083e4bacc
susu <tndus159200297@gmail.com>
2026-06-19 22:11:11 +0900

    [Wrong Answer] 두 배열의 곱 (123ms, 10MB)

---
+++ trail1/두 배열의 곱/multiple-of-two-arrays.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc  = new Scanner(System.in);
+
+        int[][] arr2dA = new int[3][3];
+        int[][] arr2dB = new int[3][3];
+
+        int num = 0;
+
+         for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr2dA[i][j] = ++num;
+                arr2dB[i][j] = num + 1;
+            }
+         }
+
+        int[][] arr2dMul = new int[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr2dMul[i][j] = arr2dA[i][j] * arr2dB[i][j];
+
+                System.out.print(arr2dMul[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
