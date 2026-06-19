9ba9aa5af6a0c3f64527b9f49e593f01db61ba78
susu <tndus159200297@gmail.com>
2026-06-19 21:59:36 +0900

    [Passed] 정수 직사각형 (181ms, 11MB)

--- trail1/정수 직사각형/rectangle-of-numbers.java
+++ trail1/정수 직사각형/rectangle-of-numbers.java
@@ -9,9 +9,13 @@ public class Main {
         int M = sc.nextInt();   // 열 개수
 
         int [][] arr2d = new int[N][M];
+
+        int num = 0;
     
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
+                arr2d[i][j] = ++num;
+
                 System.out.print(arr2d[i][j] + " ");
             }
             System.out.println();
