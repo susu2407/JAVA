dd0ac4d3cb85cd70c8802b8d32793d8d97eec7d9
susu <tndus159200297@gmail.com>
2026-06-23 11:11:43 +0900

    [Passed] 동전이 있는 위치 (135ms, 10MB)

---
+++ trail1/동전이 있는 위치/Where-coins-are-located.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int[][] arr2d = new int[N][N];  // 배열을 0으로 초기화
+
+        // 격자 위에 M개의 동전의 위치 저장
+        for (int i = 0; i < M; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            arr2d[a-1][b-1] = 1;
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (arr2d[i][j] == 1) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
