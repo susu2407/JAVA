f78fdb687bf18377eeb8060e946a4550ad918c9b
susu <tndus159200297@gmail.com>
2026-06-24 20:25:06 +0900

    [Passed] 더 긴 문자열 (132ms, 10MB)

---
+++ trail1/더 긴 문자열/longer-string.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.next();
+        String str2 = sc.next();
+
+        if (str1.length() > str2.length()) { 
+            System.out.println(str1 + " " + str1.length());
+
+        } else if (str1.length() < str2.length()) {
+            System.out.println(str2 + " " + str2.length());
+            
+        } else if (str1.length() == str2.length()) {
+            System.out.println("same");
+        }
+    }
+}
