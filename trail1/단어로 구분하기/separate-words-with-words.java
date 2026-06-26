899afcbf969c30019f5eea529340461b8960a566
susu <tndus159200297@gmail.com>
2026-06-26 22:10:42 +0900

    [Passed] 단어로 구분하기 (123ms, 10MB)

---
+++ trail1/단어로 구분하기/separate-words-with-words.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        String[] arr = new String[10];
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.next();
+
+            System.out.println(arr[i]);
+        }
+    }
+}
