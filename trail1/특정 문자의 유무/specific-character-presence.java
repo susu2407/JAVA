457a2d65aa6e5bfb17990a3ad1942227e199bab6
susu <tndus159200297@gmail.com>
2026-06-29 19:37:05 +0900

    [Passed] 특정 문자의 유무 (134ms, 10MB)

--- trail1/특정 문자의 유무/specific-character-presence.java
+++ trail1/특정 문자의 유무/specific-character-presence.java
@@ -10,10 +10,18 @@ public class Main {
         boolean exists1 = false;
         boolean exists2 = false;
 
+        // for (int i = 0; i < len - 1; i++) {
+        //     if (str.charAt(i) == 'e' && str.charAt(i + 1) == 'e') {
+        //         exists1 = true;
+        //     } else if (str.charAt(i) == 'a' && str.charAt(i + 1) == 'b') {
+        //         exists2 = true;
+        //     }
+        // }
+
         for (int i = 0; i < len - 1; i++) {
-            if (str.charAt(i) == 'e' && str.charAt(i + 1) == 'e') {
+            if (str.substring(i, i + 2).equals("ee")) {
                 exists1 = true;
-            } else if (str.charAt(i) == 'a' && str.charAt(i + 1) == 'b') {
+            } else if (str.substring(i, i + 2).equals("ab")) {
                 exists2 = true;
             }
         }
