a74690fe65d47db71d81324a4bdeda54a1629a45
susu <tndus159200297@gmail.com>
2026-06-30 15:18:01 +0900

    [Passed] 양옆 문자 제거하기 (145ms, 10MB)

---
+++ trail1/양옆 문자 제거하기/del-both-side-char.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        int len = str.length();
+
+        str = str.substring(0, 1) + str.substring(2, len - 2) + str.substring(len -1);
+
+        System.out.println(str);
+    }
+}
