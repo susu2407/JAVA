b9d38f70e793a650fc8101fd2d11c0eb0f057e24
susu <tndus159200297@gmail.com>
2026-06-30 15:34:14 +0900

    [Passed] 문자 제거하기 (135ms, 10MB)

--- trail1/문자 제거하기/del-char.java
+++ trail1/문자 제거하기/del-char.java
@@ -10,12 +10,12 @@ public class Main {
         while (str.length() > 1) {
             int n = sc.nextInt();
 
-            if (n > str.length()) {
+            if (n >= str.length()) {
                 str = str.substring(0, str.length() -1);
             } else {
                 str = str.substring(0, n) + str.substring(n + 1);
             }
-            
+
             System.out.println(str);
         }
 
