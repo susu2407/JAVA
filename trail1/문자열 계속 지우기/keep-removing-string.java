a9137c4bd0576ce9d361063afbc2bd5ef20b66bd
susu <tndus159200297@gmail.com>
2026-07-02 23:44:08 +0900

    [Passed] 문자열 계속 지우기 (131ms, 10MB)

--- trail1/문자열 계속 지우기/keep-removing-string.java
+++ trail1/문자열 계속 지우기/keep-removing-string.java
@@ -8,22 +8,28 @@ public class Main {
         String A = sc.next();
         String B = sc.next();
 
-        boolean found = true;
+        // boolean found = true;
 
-        while (found) {
-            found = false;  // 못 찻으면 멈춤. if문이 통과되어 found가 false 상태일 것이므로.
+        // while (found) {
+        //     found = false;  // 못 찻으면 멈춤. if문이 통과되어 found가 false 상태일 것이므로.
 
-            for (int i = 0; i <= A.length() - B.length(); i++) {
-                // 탐색
-                if (A.substring(i, i + B.length()).equals(B)) {
-                    // 찾으면 삭제
-                    A = A.substring(0, i) + A.substring(i + B.length());
+        //     for (int i = 0; i <= A.length() - B.length(); i++) {
+        //         // 탐색
+        //         if (A.substring(i, i + B.length()).equals(B)) {
+        //             // 찾으면 삭제
+        //             A = A.substring(0, i) + A.substring(i + B.length());
 
-                    // 찾았다는 정보 저장 & 반복 멈춤고 다시 처음부터 탐색
-                    found = true;
-                    break;
-                }
-            }
+        //             // 찾았다는 정보 저장 & 반복 멈춤고 다시 처음부터 탐색
+        //             found = true;
+        //             break;
+        //         }
+        //     }
+        // }
+
+        while (A.contains(B)) {         // contains() : B가 존재하는가
+            int index = A.indexOf(B);   // indexOf() : B가 처음 등장하는 위치(index)를 반환하는 메서드 
+
+            A = A.substring(0, index) + A.substring(index + B.length());
         }
 
         System.out.println(A);
