1db82d1f2cdebc2e50949c7d14355f400edee8b8
susu <tndus159200297@gmail.com>
2026-07-02 23:17:19 +0900

    [Passed] 문자열 계속 지우기 (128ms, 10MB)

--- trail1/문자열 계속 지우기/keep-removing-string.java
+++ trail1/문자열 계속 지우기/keep-removing-string.java
@@ -8,9 +8,21 @@ public class Main {
         String A = sc.next();
         String B = sc.next();
 
-        for (int i = 0; i < A.length() - B.length() + 1; i++) {
-            if (A.substring(i, i + B.length()).equals(B)) {
-                A = A.substring(0, i) + A.substring(i + B.length());
+        boolean found = true;
+
+        while (found) {
+            found = false;  // 못 찻으면 멈춤. if문이 통과되어 found가 false 상태일 것이므로.
+
+            for (int i = 0; i <= A.length() - B.length(); i++) {
+                // 탐색
+                if (A.substring(i, i + B.length()).equals(B)) {
+                    // 찾으면 삭제
+                    A = A.substring(0, i) + A.substring(i + B.length());
+
+                    // 찾았다는 정보 저장 & 반복 멈춤고 다시 처음부터 탐색
+                    found = true;
+                    break;
+                }
             }
         }
 
