ecde44db1b2121b0ada25e8bfbf20d1dd80b0052
susu <tndus159200297@gmail.com>
2026-07-02 22:08:03 +0900

    [Passed] 다양하게 출력 (91ms, 9MB)

---
+++ trail1/다양하게 출력/print-in-variety.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        System.out.println("Total days in Year");
+        System.out.print(365);
+        System.out.print("\n");
+        System.out.print("Circumference rate\n3.1415926535");
+    }
+}
