1001db7e5db11f925bcbc508f348cbf4f211057f
susu <tndus159200297@gmail.com>
2026-07-03 16:44:27 +0900

    [Passed] 그 다음 알파벳 (121ms, 10MB)

---
+++ trail1/그 다음 알파벳/next-alphabet.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        char a = sc.next().charAt(0);
+
+        if (a != 'z') {
+            // ver.1
+            // int b = (int)a + 1;
+            // System.out.println((char)b);
+
+            // ver.2
+            char c = (char) ((int)a + 1);
+            System.out.println(c);
+        } else {
+            System.out.println('a');
+        }
+    }
+}
