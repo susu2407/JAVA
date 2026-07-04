fbc6f56139980563e9863a0387e89b711d39d604
susu <tndus159200297@gmail.com>
2026-07-04 21:47:22 +0900

    [Passed] 대문자 소문자 바꾸기 (124ms, 10MB)

---
+++ trail1/대문자 소문자 바꾸기/change-uppercase-and-lowercase.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if ('a' <= c && c <= 'z') {
+                System.out.print((char)(c - 'a' + 'A'));
+            } else if ('A' <= c && c <= 'Z') {
+                System.out.print((char)(c - 'A' + 'a'));
+            }
+        }
+    }
+}
