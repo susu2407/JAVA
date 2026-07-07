1001e16affb196414ebdd1759cbeda7623f5049b
susu <tndus159200297@gmail.com>
2026-07-07 23:02:02 +0900

    [Passed] 일치하는 문자열의 수 (139ms, 11MB)

--- trail1/일치하는 문자열의 수/num-of-correct-string.java
+++ trail1/일치하는 문자열의 수/num-of-correct-string.java
@@ -5,6 +5,7 @@ public class Main {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
 
+        // 초기화 및 입력 받음
         int n = sc.nextInt();
         String A = sc.next();
 
@@ -14,6 +15,7 @@ public class Main {
             arr[i] = sc.next();
         }
 
+        // A와 일치한 문자열 개수의 개수 구하기
         int count = 0;
 
         for (int i = 0; i < n; i++) {
@@ -21,6 +23,7 @@ public class Main {
                 count++;
         }
 
+        // 결과 출력
         System.out.println(count);
     }
 }
