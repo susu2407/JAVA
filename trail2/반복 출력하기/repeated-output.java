0aaddf1ad8a066b9f43d69594ae5fdd300e16bd3
susu <tndus159200297@gmail.com>
2026-07-08 01:12:56 +0900

    [Passed] 반복 출력하기 (133ms, 10MB)

--- trail2/반복 출력하기/repeated-output.java
+++ trail2/반복 출력하기/repeated-output.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
+        // 변수 선언 및 입력:
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
@@ -10,6 +11,7 @@ public class Main {
         printNLine(N);
     }
 
+    // n개의 줄에 걸쳐 특정 문자열을 출력하는 함수입니다.
     public static void printNLine(int n) {
         for (int i = 0; i < n; i++) {
             System.out.println("12345^&*()_");
