b15548a0f9a0ef1724f9daf084537bf874c2563a
susu <tndus159200297@gmail.com>
2026-07-07 23:46:07 +0900

    [Wrong Answer] 미는 횟수 (124ms, 10MB)

---
+++ trail1/미는 횟수/number-of-pushes.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+         String A = sc.next();
+         String B = sc.next();
+
+         int N = -1;
+
+        for (int i = 0; i < A.length(); i++) {
+
+            A = A.substring(1) + A.substring(0, 1);     // A를 우측으로 한 칸 밀기
+                
+            if (A.equals(B)) {  // A와 B가 같아지는 순간
+                N = i + 1;      // A를 민 횟수
+
+                break;
+            }
+        }
+
+        // 결과 출력
+        System.out.println(N);
+    }
+}
