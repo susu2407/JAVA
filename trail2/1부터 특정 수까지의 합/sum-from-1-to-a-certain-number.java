e4b4bd2688f519857c43c7821dd6adc4eb85284c
susu <tndus159200297@gmail.com>
2026-07-11 23:42:44 +0900

    [Passed] 1부터 특정 수까지의 합 (126ms, 10MB)

---
+++ trail2/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        System.out.println(func(n));
+    }
+
+    public static int func (int n) {
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        return sum / 10;
+    }
+}
