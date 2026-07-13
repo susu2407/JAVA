92f6078f5cd844eb202cf6ad5313bf0b30cf5f59
susu <tndus159200297@gmail.com>
2026-07-13 21:18:09 +0900

    [Passed] 짝수이면서 합이 5의 배수인 수 (131ms, 10MB)

--- trail2/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.java
+++ trail2/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.java
@@ -16,24 +16,16 @@ public class Main {
 
     public static boolean isEvenAndDigitSumMultipleOfFive(int n) {
         /*
-            1. n이 짝수인지 판단
-            2. 짝수라면, 각 자리 숫자의 합(sum) 구하기
-            3. sum이 5의 배수인지 판단
+            1. n의 각 자리 숫자 분리
+            2. 각 자리 숫자의 합(sum) 구하기
+            3. n이 짝수인지 판단 && sum이 5의 배수인지 판단
         */
 
-        int sum = 0;
+        int a = n / 10;
+        int b = n % 10;
 
-        if (n % 2 == 0) {
-            int a = n / 10;
-            int b = n % 10;
+        int sum = a + b;
 
-            sum = a + b;
-
-            if (sum % 5 ==0) {
-                return true;
-            }
-        }
-
-        return false;
+        return (n % 2 == 0) && (sum % 5 == 0);
     }
 }
