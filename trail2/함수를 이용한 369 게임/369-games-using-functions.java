d8cb450a98a8a10de24b47517597b3aa7e419944
susu <tndus159200297@gmail.com>
2026-07-13 21:44:53 +0900

    [Passed] 함수를 이용한 369 게임 (143ms, 12MB)

--- trail2/함수를 이용한 369 게임/369-games-using-functions.java
+++ trail2/함수를 이용한 369 게임/369-games-using-functions.java
@@ -11,11 +11,16 @@ public class Main {
         int count = 0;
         
         for (int i = A; i <= B; i++) {
-            if (hasDigit369(i) || isMulipleOfThree(i))
+            if (is369Number(i))
                 count++;
         }
 
         System.out.println(count);
+    }
+
+    // 3의 배수인가 판단
+    public static boolean is369Number(int i) {
+        return (i % 3 == 0 || hasDigit369(i));
     }
 
     // 3, 6, 9 중에 하나가 들어가 있는지 판단
@@ -23,18 +28,12 @@ public class Main {
         while (i != 0) {
             int digit = i % 10;
 
-            if (digit == 3 || digit == 6 || digit == 9) {
+            if (digit == 3 || digit == 6 || digit == 9)
                 return true;
-            }
 
             i = i / 10;
         }
 
         return false;
-    }
-
-    // 3의 배수인가 판단
-    public static boolean isMulipleOfThree(int i) {
-        return i % 3 == 0;
     }
 }
